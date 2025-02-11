caa65cd2213b4e0ea7bffac121061adf4d6c4703
Leandro Shiniti Tacara <leandro.tacara@hotmail.com>
2025-02-10 21:28:10 -0300

    add gitflow

--- src/main/java/br/com/fiap/soat7/infrastructure/services/kubernetes/KubernetesService.java
+++ src/main/java/br/com/fiap/soat7/infrastructure/services/kubernetes/KubernetesService.java
@@ -46,6 +46,7 @@ public class KubernetesService {
 					.kind("Pod")
 					.metadata(new V1ObjectMeta().name(podName).namespace(props.getK8s().getNamespace()))
 					.spec(new V1PodSpec()
+							.serviceAccount("my-app-sa")
 							.restartPolicy("Never")
 							.addContainersItem(new V1Container()
 									.name(podName)
